f92d4197611edf7e09034343448a2213e7ce6cc5
Vasile Cebotari <cebotarivasia4@gmail.com>
2025-01-07 12:55:41 +0200

    Implemented an update for posts with an editing form

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class EditController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(Post $post, Category $category, Tag $tag)
     {
-        return view('admin.posts.edit', compact('post'));
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.posts.edit', compact('post', 'categories', 'tags'));
     }
 }
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -28,17 +28,87 @@
                 <!--begin::Row-->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.post.update', $post->id ) }}" method="POST" class="w-25">
+                        <form action="{{ route('admin.post.update', $post->id ) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('PATCH')
-                            <div class="mb-3">
+                            <div class="form-group mb-3 w-25">
                                 <label class="form-label">Title</label>
                                 <input type="text" class="form-control" name="title" placeholder="Post title" value="{{ $post->title }}">
                                 @error('title')
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-secondary mb-2" value="Update">
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">{{ $post->content }}</textarea>
+                                @error('content')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="input-group mt-3 w-50">
+                                <label class="form-label">Add preview</label>
+                                <div class="container">
+                                    <div class="row w-50 mb-2">
+                                        <img src="{{ asset('storage/' . $post->preview_image) }}" alt="preview_image" class="d-block w-50">
+                                    </div>
+                                </div>
+
+                                <div class="input-group">
+                                    <input type="file" class="form-control" id="inputGroupFile02" name="preview_image">
+                                    <label class="input-group-text" for="inputGroupFile02">Upload</label>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="input-group mt-3 w-50">
+                                <label class="form-label">Add main preview</label>
+                                <div class="container">
+                                    <div class="row w-50 mb-2">
+                                        <img src="{{ asset('storage/' . $post->main_image) }}" alt="main_image" class="d-block w-50">
+                                    </div>
+                                </div>
+                                <div class="input-group">
+                                    <input type="file" class="form-control" id="inputGroupFile02" name="main_image">
+                                    <label class="input-group-text" for="inputGroupFile02">Upload</label>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group mt-3 w-50">
+                                <label class="form-label">Select category</label>
+                                <select name="category_id" class="form-select">
+                                    <option>Choose...</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}" {{ $category->id == $post->category_id ? ' selected' : '' }}>
+                                            {{ $category->title }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
+                                <div class="text-danger">{{ 'The selected category is invalid.' }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group mt-3">
+                                <label class="form-label">Select tags</label>
+                                <div mbsc-page class="demo-multiple-select">
+                                    <div style="height:100%; width: 50%">
+                                        <label>
+                                            <input mbsc-input id="demo-multiple-select-input" placeholder="Please select..." data-dropdown="true" data-input-style="outline" data-label-style="stacked" data-tags="true" />
+                                        </label>
+                                        <select id="demo-multiple-select" name="tag_ids[]" multiple>
+                                            @foreach($tags as $tag)
+                                                <option value="{{ $tag->id }}" {{ is_array( $post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : '' }}>
+                                                    {{ $tag->title }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-secondary mb-2 mt-3" value="Update">
+                            </div>
                         </form>
                     </div>
                 </div>

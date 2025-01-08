e2cc6c333def923257366649a3ddfffde2d088d9
Vasile Cebotari <cebotarivasia4@gmail.com>
2025-01-08 12:29:27 +0200

    Created a Service class to refactor creation and update of posts

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -8,35 +8,13 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
-        $tagIds = $data['tag_ids'];
-        unset($data['tag_ids']);
+        $post = $this->service->update($data, $post, $request);
 
-        // Обработка preview_image
-        if ($request->hasFile('preview_image')) {
-            // Если загружен новый файл, сохраняем его
-            $data['preview_image'] = Storage::disk('public')->put('/images', $request->file('preview_image'));
-        } else {
-            // Если файл не был загружен, сохраняем старое значение (из базы данных)
-            $data['preview_image'] = $post->preview_image;
-        }
-
-        // Обработка main_image
-        if ($request->hasFile('main_image')) {
-            // Если загружен новый файл, сохраняем его
-            $data['main_image'] = Storage::disk('public')->put('/images', $request->file('main_image'));
-        } else {
-            // Если файл не был загружен, сохраняем старое значение (из базы данных)
-            $data['main_image'] = $post->main_image;
-        }
-
-
-        $post->update($data);
-        $post->tags()->sync($tagIds);
         return redirect()->route('admin.post.show', compact('post'));
     }
 }
---
+++ app/Service/PostService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service;
+
+use App\Http\Requests\Admin\Tag\UpdateRequest;
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    public function store($data) {
+        try {
+            DB::beginTransaction();
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tagIds);
+            DB::commit();
+        }catch (\Exception $exception){
+            DB::rollBack();
+            abort(500);
+        }
+    }
+
+    public function update($data, $post, $request) {
+        try {
+        DB::beginTransaction();
+        $tagIds = $data['tag_ids'];
+        unset($data['tag_ids']);
+
+        $request->hasFile('preview_image')
+            ? $data['preview_image'] =  Storage::disk('public')->put('/images', $request->file('preview_image'))
+            : $data['preview_image'] =  $post->preview_image;
+
+        $request->hasFile('main_image')
+            ? $data['main_image'] = Storage::disk('public')->put('/images', $request->file('main_image'))
+            : $data['main_image'] = $post->main_image;
+
+        $post->update($data);
+        $post->tags()->sync($tagIds);
+        DB::commit();
+        }catch (\Exception $exception){
+            DB::rollBack();
+            abort(500);
+        }
+
+        return $post;
+    }
+
+}
